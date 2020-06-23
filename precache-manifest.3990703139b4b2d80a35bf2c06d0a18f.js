6e93b0e5fc2bceb0fa828f3f671a55e3806c75ea
박대규 <mamma1234@gmail.com>
2020-06-23 17:23:12 +0900

    Updates

---
+++ precache-manifest.3990703139b4b2d80a35bf2c06d0a18f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6646f8466613bc732856554bb85e249",
+    "url": "/mamma1234/mamma1234.github.io.sub/index.html"
+  },
+  {
+    "revision": "946b834f96d9c235add6",
+    "url": "/mamma1234/mamma1234.github.io.sub/static/css/main.8ce4658d.chunk.css"
+  },
+  {
+    "revision": "9a6a2b1b8a64d1478ff3",
+    "url": "/mamma1234/mamma1234.github.io.sub/static/js/2.68e17161.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/mamma1234/mamma1234.github.io.sub/static/js/2.68e17161.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "946b834f96d9c235add6",
+    "url": "/mamma1234/mamma1234.github.io.sub/static/js/main.1868e7ff.chunk.js"
+  },
+  {
+    "revision": "69dc74e78517053d57d0",
+    "url": "/mamma1234/mamma1234.github.io.sub/static/js/runtime-main.17011c76.js"
+  }
+]);

33bf62830f2e667bd84752b0dba6480f9c52e108
박대규 <mamma1234@gmail.com>
2020-06-23 17:42:48 +0900

    update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,7 +5,7 @@ import "./Navigation.css";
 function Navigation() {
   return (
     <div className="nav">
-        <col>gh-pages</col>
+        <h3>gh-pages</h3>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
     </div>
